1e0309cda63951f98ff69f9298b6d198380352f9
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-15 05:05:42 +0200

    Trabajando en el login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,13 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './../backend/css/sb-admin-2.min.css';
-
+import {auth} from './../firebase/firebaseConfig';
+import {useNavigate} from 'react-router-dom';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
 
   useEffect(() => {
@@ -10,8 +15,42 @@ const Login = () => {
   }, []);
 
 
+  const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    return (
+    try {
+			await signInWithEmailAndPassword(auth, email, password);
+			navigate('/dashboard');
+		} catch(error) {
+			console.log(error)
+			// cambiarEstadoAlerta(true);
+			let mensaje;
+			switch(error.code){
+				case 'auth/wrong-password':
+					mensaje = 'La contraseña no es correcta.'
+					break;
+				case 'auth/user-not-found':
+					mensaje = 'No se encontro ninguna cuenta con este correo electrónico.'
+					break;
+				default:
+					mensaje = 'Hubo un error al intentar crear la cuenta.'
+				break;
+			}
+
+			// cambiarAlerta({tipo: 'error', mensaje: mensaje});
+		}
+
+  }
+
+  const handleChange = (e) => {
+		if(e.target.name === 'email'){
+			setEmail(e.target.value);
+		} else if (e.target.name === 'password'){
+			setPassword(e.target.value);
+		}
+	}
+
+  return (
         <>
           <div class="container">
 
@@ -29,17 +68,30 @@ const Login = () => {
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">XFinca</h1>
               </div>
-              <form class="user">
+              <form class="user" onSubmit={handleSubmit}>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..."/>
+                      <input  type="email" 
+                              class="form-control form-control-user" 
+                              id="email" 
+                              aria-describedby="emailHelp" 
+                              placeholder="Correo Electrónico"
+                              name="email"
+                              value={email}
+					                    onChange={handleChange}
+                              />
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password"/>
+                      <input  type="password" class="form-control form-control-user" 
+                              id="password"
+                              placeholder="Password"
+                              name="password"
+                              value={password}
+					                    onChange={handleChange}
+                  />
                 </div>
-                
-                <a href="index.html" class="btn btn-primary btn-user btn-block">
-                  Registrarse
-                </a>
+
+                <button class="btn btn-primary btn-user btn-block" type="submit">Iniciar Sesión</button>
+
               </form>
             </div>
           </div>
