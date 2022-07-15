d8337d2a941bce192306cf602dd8b0bdf00f541e
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-15 03:57:04 +0200

    SC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+
 function App() {
   return (
     <div className="App">
-        <h1>Hello</h1>
+        <h1>Home</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dasboard from './components/Dasboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <BrowserRouter>
+      <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/Dashboard" element={<Dasboard/>}/>
+            <Route path="/" element={<App/>}/> 
+      </Routes>
+    </BrowserRouter>
+  </>
 );
