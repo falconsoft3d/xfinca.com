bf1967b56c3bb45209169ad4a18608a8f98b59be
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-07-16 01:04:03 +0200

    SX

--- src/components/BackendFooter.js
+++ src/components/BackendFooter.js
@@ -5,7 +5,7 @@ const BackendFooter = () => {
     <footer class="sticky-footer bg-white">
         <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
+            <span>Copyright &copy; wwww.marlonfalcons.com 2022</span>
         </div>
         </div>
   </footer> );
